18faca24a685bbf9e373dd7b063bfd0aea1e6d55
Aayush Mehta <aayushmehta5225@gmail.com>
2023-03-25 13:42:28 +0530

    array delete from last

--- text-rotator-test/script.js
+++ text-rotator-test/script.js
@@ -1,19 +1,68 @@
-const spanEl = document.querySelector('.span')
-const word = 'first'
+const spanEl = document.querySelector(".span");
+let word = "first";
 
-const arr = ['f']
+const wordArr = [...word];
+// console.log(wordArr);
 
-const wordArr = [...word]
-console.log(wordArr)
-
-let count = 0
+let count = 0;
 
 function rotateText() {
-  spanEl.textContent += wordArr[count]
-  count++
+  spanEl.textContent += wordArr[count];
+  count++;
   if (count === word.length) {
-    clearInterval(intervalId)
+    clearInterval(intervalId);
   }
 }
 
-const intervalId = setInterval(rotateText, 200)
+const intervalId = setInterval(rotateText, 300);
+// wordArr.splice(-1, 1);
+console.log(spanEl.textContent);
+
+function deletee() {
+  // spanEl.textContent = wordArr.splice(-1, 1);
+  wordArr.splice(-1, 1);
+  console.log(wordArr);
+}
+
+// for (let i = word.length - 1; i > 0; i--) {
+//   if (wordArr.length > 0) {
+//     wordArr.splice(-1, 1);
+//     console.log(wordArr.join(""));
+//     // spanEl.textContent -= wordArr;
+//   }
+// }
+
+deletee();
+
+// wordArr.splice(`${word.length - 1}`, 1);
+// word.length--;
+// console.log(wordArr);
+
+// wordArr.splice(`${word.length - 1}`, 1);
+// console.log(wordArr);
+
+// for (let i = 0; i < word.length; i++) {
+//   word = word.substring(1);
+// }
+// console.log(word);
+
+// for (let i = word.length - 1; i > 0; i--) {
+//   if (wordArr.length > 0) {
+//     wordArr.splice(-1, 1);
+//     console.log(wordArr.join(""));
+//     spanEl.textContent -= wordArr;
+//   }
+// }
+
+// tempLen = word.length;
+// word.length = word.length - 1;
+
+// wordArr.splice(word.length, 1);
+// console.log(word.length);
+
+// wordArr.splice(`${word.length - 1}`, 1);
+// word.length--;
+// console.log(wordArr);
+// wordArr.splice(`${word.length - 1}`, 1);
+// word.length--;
+// console.log(wordArr);
