660e6f4b9af334047f5c9b567222d7cba32a6a14
AayushMehta <aayushmehta5225@gmail.com>
2023-03-29 14:04:47 +0530

    mapty

--- functions-prac/script.js
+++ functions-prac/script.js
@@ -77,7 +77,7 @@ const upperFirstWord = function (str) {
   return [first.toUpperCase(), ...others].join(' ');
 };
 
-// Higher-order  function
+// Higher-order   function
 const transformer = function (str, fn) {
   console.log(`Original string: ${str}`);
   console.log(`Transformed string: ${fn(str)}`);
